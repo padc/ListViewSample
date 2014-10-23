e88a920753b694abc0dc899f1585672cc5de3c05
Lope Emano <lopegwapo@gmail.com>
2014-10-23 15:02:57 +0800

    init commit

---
+++ app/src/main/java/listview/sample/padc/github/io/listviewexample/MainActivity.java
@@ -0,0 +1,17 @@
+package listview.sample.padc.github.io.listviewexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
